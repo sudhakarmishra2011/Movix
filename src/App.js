5233523ca09882914c0a1877ea3a46b9001228c1
Sudhakar <sudhakarmishra2011@gmail.com>
2023-07-12 12:08:56 +0530

    Added Hero Banner on HomePage

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useEffect } from 'react';
+import { fetchDataFromApi } from './utils/api'
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration, getGenres } from './store/homeSlice';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home'
+import Details from './pages/details/Details';
+import SearchResult from './pages/searchResult/SearchResult'
+import Explore from './pages/explore/Explore';
+import PageNotFound from './pages/404/PageNotFound';
+
+function App() {
+  const dispatch = useDispatch()
+
+  const { url } = useSelector((state) => state.home)
+  console.log(url)
+
+  useEffect(() => {
+    fetchApiConfiguration();
+  }, [])
+
+  const fetchApiConfiguration = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      const url = {
+        backdrop : res.images.secure_base_url + "original",
+        poster : res.images.secure_base_url + "original",
+        profile : res.images.secure_base_url + "original",
+      }
+      dispatch(getApiConfiguration(url));
+    });
+  }
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route>
+          <Route path='/' element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Details />} />
+          <Route path="/search/:query" element={<SearchResult />} />
+          <Route path="/explore/:mediaType" element={<Explore />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import "./style.scss";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
+
+const Header = () => {
+    const [show, setShow] = useState("top");
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const [query, setQuery] = useState("");
+    const [showSearch, setShowSearch] = useState("");
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    return (
+        <div>Header</div>
+    );
+};
+
+export default Header;
